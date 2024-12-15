eeea8345f37218014185a4341bc30616a6e97546
Fattan <fattanmalva14@gmail.com>
2024-12-15 17:19:43 +0700

    [ADD]Seeder Customer

--- database/migrations/2024_12_14_150403_create_customers_table.php
+++ database/migrations/2024_12_14_150403_create_customers_table.php
@@ -12,12 +12,13 @@ return new class extends Migration {
     {
         Schema::create('customer', function (Blueprint $table) {
             $table->id();
-            $table->string('username'); 
-            $table->string('password'); 
-            $table->string('role'); 
-            $table->string('name'); 
-            $table->timestamps(); 
+            $table->string('username');
+            $table->string('password');
+            $table->string('role');
+            $table->string('name');
+            $table->timestamps();
         });
+        Artisan::call('db:seed', ['--class' => 'CustomerSeeder']);
     }
 
     /**
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('customer')->insert([
+            'username' => 'edp@gmail.com',
+            'password' => Hash::make('jangandihack'),
+            'role' => 'admin',
+            'name' => 'EDP admin',
+        ]);
+    }
+}
